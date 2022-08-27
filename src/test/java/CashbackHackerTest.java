e0f4ec175287ca49a0eac98cf15e0b0c3d49470f
Абдуллин Рушан <rushan.lucky@yandex.ru>
2022-08-27 14:14:32 +0500

    JUnit4 tests

--- src/test/java/CashbackHackerTest.java
+++ src/test/java/CashbackHackerTest.java
@@ -1,7 +1,7 @@
-import static org.testng.Assert.*;
+import org.junit.Assert;
 
 public class CashbackHackerTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void remain() {
         CashbackHacker service = new CashbackHacker();
         int amount = 800;
@@ -9,17 +9,17 @@ public class CashbackHackerTest {
         int expected = 200;
 
 
-        assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
 
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void calculateOneThousand(){
         CashbackHacker service = new CashbackHacker();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected =0;
 
-        assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
 }
